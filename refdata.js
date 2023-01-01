1144e1e327f360f9daeda401cecd6b3b09270267
MAYANK PUVVALA <104277096+mayankpuvvala@users.noreply.github.com>
2023-01-02 00:18:29 +0530

    Add files via upload

---
+++ refdata.js
@@ -0,0 +1,22 @@
+//object form'n
+let info={
+    naam: "mayank",
+    roll: 616
+}
+console.log(info.naam);
+console.log(info["naam"]);
+console.log(info.roll);
+
+// arrays
+let numbers= ["1", "2", "3"];
+console.log(numbers);
+console.log(numbers[1]);
+console.log(numbers.length);
+
+//functions
+let str= "this is for functions";
+console.log(str);
+function add(a,b){
+    return a+b;
+}
+console.log(add(2,6));
